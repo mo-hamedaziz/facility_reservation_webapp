b187adf367146797e159ab41b3b85e2b62db66a7
Mohamed Aziz Bchini <mohamedaziz.bchini@insat.ucar.tn>
2024-03-28 11:43:45 +0100

    Creating an empty main branch

--- client/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return ( 
-        <>
-            <h1>Footer Goes Here</h1>
-        </>
-     );
-}
- 
-export default Footer;
--- client/src/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-    return ( 
-        <>
-            <h1>Navbar Goes Here</h1>
-        </>
-     );
-}
- 
-export default Navbar;
