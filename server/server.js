9419f50b93f80b0c34505c6039a58ee21a026173
Mohamed Aziz Bchini <mohamedaziz.bchini@insat.ucar.tn>
2024-04-04 16:17:28 +0100

    Setting up the express app+ models

--- server/server.js
+++ server/server.js
@@ -1,26 +1,20 @@
+require("dotenv").config();
 const express = require("express");
-const mongoose = require('mongoose');
-const cors = require("cors");
+const mongoose = require("mongoose");
+
+const port = process.env.PORT;
 
 const app = express();
 
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-
-app.use(cors());
-
-app.use(express.json());
-
-app.use(require("./routes/record"));
-
-// Get MongoDB driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // Perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+// Connect to DB
+console.log('Connecting to the database ...');
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listen for requests
+        app.listen(port, () => {
+            console.log(`Connected to the Database\nListening on port: ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
