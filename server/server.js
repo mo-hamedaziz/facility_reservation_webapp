c9df4066709a000d75d7a1cf5eb4a46cd3bb6adf
ineszghal02 <ines.zghal@insat.ucar.tn>
2024-04-13 18:27:38 +0100

    commit message

--- server/server.js
+++ server/server.js
@@ -1,26 +1,31 @@
+require("dotenv").config();
 const express = require("express");
-const mongoose = require('mongoose');
-const cors = require("cors");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/dashboardRoutes");
+
+const port = process.env.PORT;
 
 const app = express();
 
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-
-app.use(cors());
-
+// Using middleware
 app.use(express.json());
-
-app.use(require("./routes/record"));
-
-// Get MongoDB driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // Perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
 });
+
+// Dashboard Routes
+app.use('/dashboard',workoutRoutes);
+
+// Connect to DB
+console.log('Connecting to the database ...');
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listen for requests
+        app.listen(port, () => {
+            console.log(`Connected to the Database\nListening on port: ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
