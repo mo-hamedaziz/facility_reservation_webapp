56647433a09bd1a97991bbe8877ec68c138a8000
Louay <louay.mejri44@gmail.com>
2024-05-30 15:26:13 +0100

    footer

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -1,9 +1,24 @@
+import React from 'react';
+import './Footer.css';
+
 const Footer = () => {
-    return ( 
-        <>
-            <h1>Footer Goes Here</h1>
-        </>
-     );
+    return (
+        <div className="footer-container">
+            <div className="footer-logo">
+                <i className="bi bi-copyright"></i>
+            </div>
+            <div className="footer-elements">
+                <ul>
+                    <li>
+                        <a href="/">Facility Reservation</a>
+                    </li>
+                    <li>
+                        <a href="/">2023-2024</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
- 
+
 export default Footer;
